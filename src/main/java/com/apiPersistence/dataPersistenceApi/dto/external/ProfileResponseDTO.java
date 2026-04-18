6175e6cb65f59dfcd103898fe4a2617847cd10b9
Etoro Umoren <torohkiss@yahoo.com>
2026-04-18 21:30:27 +0100

    updated properties and dto

--- src/main/java/com/apiPersistence/dataPersistenceApi/dto/external/ProfileResponseDTO.java
+++ src/main/java/com/apiPersistence/dataPersistenceApi/dto/external/ProfileResponseDTO.java
@@ -1,16 +1,32 @@
 package com.apiPersistence.dataPersistenceApi.dto.external;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.time.Instant;
 import java.util.UUID;
 
-public record ProfileResponseDTO(UUID id,
-                                 String name,
-                                 String gender,
-                                 Double genderProbability,
-                                 Integer sampleSize,
-                                 Integer age,
-                                 String ageGroup,
-                                 String countryId,
-                                 Double countryProbability,
-                                 Instant createdAt) {
-}
+public record ProfileResponseDTO(
+        UUID id,
+        String name,
+        String gender,
+
+        @JsonProperty("gender_probability")
+        Double genderProbability,
+
+        @JsonProperty("sample_size")
+        Integer sampleSize,
+
+        Integer age,
+
+        @JsonProperty("age_group")
+        String ageGroup,
+
+        @JsonProperty("country_id")
+        String countryId,
+
+        @JsonProperty("country_probability")
+        Double countryProbability,
+
+        @JsonProperty("created_at")
+        Instant createdAt
+) {}
